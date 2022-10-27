dd0c361f4c79f52228eadd0bc1035881a583de0e
GottaGinger <sierratmorris@gmail.com>
2022-10-27 17:49:40 -0400

    decision making

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,15 @@ public class Main {
             System.out.println("Enter a number between 1 and 100");
             int num = input.nextInt();
             input.nextLine();
-            if (num % 2 == 1 && num > 60 && num < 100) {
+            if ((num % 2 == 1) && (num > 60) && (num < 100)) {
                 System.out.println(num + " Odd and over 60.");
-            } else if (num % 2 == 1 && num > 0 && num < 60)  {
+            } else if ((num % 2 == 1) && (num > 0) && (num < 60))  {
                 System.out.println(num + " and Odd");
-            } else if (num % 2 == 0 && num > 60 && num <= 100) {
+            } else if ((num % 2 == 0) && (num > 60) && (num <= 100)) {
                 System.out.println(num + " Even");
-            } else if (num % 2 == 0 && num >= 26 && num <= 60) {
+            } else if ((num % 2 == 0) && (num >= 26) && (num <= 60)) {
                 System.out.println("Even");
-            } else if (num % 2 == 0 && num >= 2 && num <= 25) {
+            } else if ((num % 2 == 0) && (num >= 2) && (num <= 25)) {
                 System.out.println("Even and less than 25");
             }
             System.out.println("Continue? (y/n)");
